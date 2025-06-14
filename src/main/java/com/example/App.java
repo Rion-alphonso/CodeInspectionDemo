fea9dee0bebe858093c7607659839e95e4be17d9
Rion Alphonso <rionalphonso4@gmail.com>
2025-06-15 00:29:27 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,13 @@
-package com.rion;
+package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+@SpringBootApplication
+public class CodeInspectionApplication {
+ 
+	public static void main(String[] args) {
+		SpringApplication.run(CodeInspectionApplication.class, args);
+	}
+ 
 }
